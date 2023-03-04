5622a8dde30da7bd42bd49402ee7ce56f61ebce6
wenmin-wu <wuwenmin1991@gmail.com>
2023-03-04 20:13:09 +0800

    fix the role bug

--- main.py
+++ main.py
@@ -5,35 +5,41 @@ from flask import Flask, request, render_template, redirect
 
 server = Flask(__name__, static_url_path="/static")
 
-messages = []
-
 N = 20
 
+class Conversation:
+    def __init__(self, prompt, num_of_round):
+        self.prompt = prompt
+        self.num_of_round = num_of_round
+        self.messages = []
+        self.messages.append({"role": "system", "content": self.prompt})
 
-def generate_response(prompt):
-    global messages
-    try:
-        messages.append({"role": "user", "content": prompt})
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=0.7,
-            max_tokens=2048,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=None,
-        )
-    except Exception as e:
+    def ask(self, question):
+        try:
+            self.messages.append({"role": "user", "content": question})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.messages,
+                temperature=0.5,
+                max_tokens=2048,
+                top_p=1,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+            )
+        except Exception as e:
+            print(e)
+            return e
 
-        print(e)
-        return e
-    message = response["choices"][0]["message"]["content"]
-    messages.append({"role": "system", "content": message})
-    if len(messages) > N:
-        messages = messages[-N:]
-    return message
+        message = response["choices"][0]["message"]["content"]
+        self.messages.append({"role": "assistant", "content": message})
 
+        if len(self.messages) > self.num_of_round * 2 + 1:
+            del self.messages[1:3]
+        return message
+
+        
+
+converstation = Conversation(prompt="You are a helpful assistant.", num_of_round=10)
 
 @server.route("/chat")
 def home():
@@ -44,10 +50,8 @@ def home():
 def get_bot_response():
     user_text = request.args.get("msg")
     return markdown2.markdown(
-        str(generate_response(user_text)),
-        extras=["fenced-code-blocks"] 
+        str(converstation.ask(user_text)), extras=["fenced-code-blocks"]
     )
-
 
 
 if __name__ == "__main__":
